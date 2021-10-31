90c918a8e9af56898eaf5d941393974a686b7691
AlexEntis <50030261+entis228@users.noreply.github.com>
2021-10-31 17:29:37 +0300

    add raw unit15

---
+++ unit_15_servlets/src/main/java/com/alevel/jeebox/IpSaverServlet.java
@@ -0,0 +1,47 @@
+package com.alevel.jeebox;
+
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.concurrent.ConcurrentHashMap;
+
+@WebServlet(name = "ipsaver-servlet", urlPatterns = "/")
+public class IpSaverServlet extends HttpServlet {
+
+    private static final ConcurrentHashMap<String, String>loggedUsers=new ConcurrentHashMap<>();
+
+    private static final long serialVersionUID = -8948379822734246922L;
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(IpSaverServlet.class);
+
+    @Override
+    public void init() {
+        LOGGER.info(getServletName() + " initialized");
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        PrintWriter responseBody = resp.getWriter();
+        String clientIp=req.getRemoteHost();
+        String userAgent=req.getHeader("User-agent");
+        loggedUsers.put(clientIp,userAgent);
+        resp.setContentType("text/html");
+        responseBody.println("<h1 align=\"center\">All visited users</h1>");
+        loggedUsers.forEach((x,y)->{
+            if(x.equals(clientIp))
+            responseBody.println("<h3 align=\"center\"><br>%s %s</br></h3>".formatted(x,y));
+            else responseBody.println("<h3 align=\"center\">%s %s</h3>".formatted(x,y));
+        });
+    }
+
+    @Override
+    public void destroy() {
+        LOGGER.info(getServletName() + " destroyed");
+    }
+}
